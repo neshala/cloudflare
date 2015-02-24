d26be3222be8a32681168c87cade2868497b8c5c
Nenad Milosavljevic <lazarevac@gmail.com>
2015-02-24 10:50:49 +0100

    PSR2 Update

--- cloudflare.php
+++ cloudflare.php
@@ -13,115 +13,116 @@ $cf = new cloudFlare();
 $result = $cf->setDNSRecords();
 echo $result != '' ? "Updated: {$result}" : "Nothing updated";
 
-class cloudFlare {
+class cloudflare
+{
+    private $cf_url        = 'https://www.cloudflare.com/api_json.html';
+    private $cf_user       = 'email@gmail.com';
+    private $cf_api_key    = '123456';
+    private $domain        = 'domain.info';
+    private $service_mode  = 1;
+    private $ttl           = 1;
+    private $cf_dns_id     = array('*.domain.info', 'pi.domain.info');
+    private $my_current_ip = '';
 
-	private $cf_url        = 'https://www.cloudflare.com/api_json.html';
-	private $cf_user       = 'email@gmail.com';
-	private $cf_api_key    = '123456';
-	private $domain        = 'domain.info';
-	private $service_mode  = 1;
-	private $ttl           = 1;
-	private $cf_dns_id     = array('*.domain.info', 'pi.domain.info');
-	private $my_current_ip = '';
+    public function __construct()
+    {
+        $this->my_current_ip = $this->curl('http://myip.dnsomatic.com/');
+    }
 
-	function __construct()
-	{
-		$this->my_current_ip = $this->curl('http://myip.dnsomatic.com/');
-	}
+    // get dns records and update them if needed
+    public function setDNSRecords()
+    {
 
-	// get dns records and update them if needed
-	public function setDNSRecords() {
-		
-		// first get list of dns records and check is update needed
-		$dns_records = $this->getDNSRecords();
+        // first get list of dns records and check is update needed
+        $dns_records = $this->getDNSRecords();
 
-		$updated = '';
-		foreach ($dns_records as $k => $v) {
+        $updated = '';
+        foreach ($dns_records as $k => $v) {
+            if (isset($v['content']) && $v['content'] != $this->my_current_ip) {
+                //dns ip is different than current ip, update needed...
+                if ($this->updateDNS($v)) {
+                    $updated .= "{$v['name']}, ";
+                }
+            }
+        }
 
-			if(isset($v['content']) && $v['content'] != $this->my_current_ip) {
-				//dns ip is different than current ip, update needed...
-				if($this->updateDNS($v)) {
-					$updated .= "{$v['name']}, ";
-				}
-			}
-		}
-		
-		$updated = trim($updated, ", ");
-		return $updated;
-	}
+        $updated = trim($updated, ", ");
 
-	// update single DNS record
-	private function updateDNS($data = array() )
-	{
-		$params                 = array();
-		$params['a']            = 'rec_edit';
-		$params['tkn']          = $this->cf_api_key;
-		$params['id']           = $data['rec_id'];
-		$params['email']        = $this->cf_user;
-		$params['z']            = $this->domain;
-		$params['type']         = 'A';
-		$params['name']         = $data['name'];
-		$params['content']      = $this->my_current_ip;
-		$params['service_mode'] = $this->service_mode;
-		$params['ttl']          = $this->ttl;
+        return $updated;
+    }
 
-		$res = $this->curl($this->cf_url, $params);
-		$res = json_decode($res, true);
+    // update single DNS record
+    private function updateDNS($data = array())
+    {
+        $params                 = array();
+        $params['a']            = 'rec_edit';
+        $params['tkn']          = $this->cf_api_key;
+        $params['id']           = $data['rec_id'];
+        $params['email']        = $this->cf_user;
+        $params['z']            = $this->domain;
+        $params['type']         = 'A';
+        $params['name']         = $data['name'];
+        $params['content']      = $this->my_current_ip;
+        $params['service_mode'] = $this->service_mode;
+        $params['ttl']          = $this->ttl;
 
-		return isset($res['result']) && $res['result'] == 'success' ? true : false;
-	}
+        $res = $this->curl($this->cf_url, $params);
+        $res = json_decode($res, true);
 
-	// return ID's of DNS records specified in $this->cf_dns_id
-	private function getDNSRecords() {
+        return isset($res['result']) && $res['result'] == 'success' ? true : false;
+    }
 
-		$params          = array();
-		$params['a']     = 'rec_load_all';
-		$params['tkn']   = $this->cf_api_key;
-		$params['email'] = $this->cf_user;
-		$params['z']     = $this->domain;
+    // return ID's of DNS records specified in $this->cf_dns_id
+    private function getDNSRecords()
+    {
+        $params          = array();
+        $params['a']     = 'rec_load_all';
+        $params['tkn']   = $this->cf_api_key;
+        $params['email'] = $this->cf_user;
+        $params['z']     = $this->domain;
 
-		$data = $this->curl($this->cf_url, $params);
-		$data = json_decode($data, true);
+        $data = $this->curl($this->cf_url, $params);
+        $data = json_decode($data, true);
 
-		if(!isset($data['response']['recs']['objs']))
-			return false;
+        if (!isset($data['response']['recs']['objs'])) {
+            return false;
+        }
 
-		$data = $data['response']['recs']['objs'];
-		$result = array();
+        $data = $data['response']['recs']['objs'];
+        $result = array();
 
-		foreach ($data as $k => $v) {
+        foreach ($data as $k => $v) {
+            if (!in_array($v['name'], $this->cf_dns_id)) {
+                continue;
+            }
 
-			if(!in_array($v['name'], $this->cf_dns_id))
-				continue;
+            $result[] = $v;
+        }
 
-			$result[] = $v;
-		}
+        return $result;
+    }
 
-		return $result;
-	}
+    // fetch remote data via curl
+    private function curl($url = '', $params = array())
+    {
+        if ($url == '') {
+            return false;
+        }
 
-	// fetch remote data via curl
-	private function curl($url = '', $params = array()) {
-		if($url == '')
-			return false;
+        $ch = curl_init();
 
-		$ch = curl_init();
-		 
-		curl_setopt_array($ch, array(
-		    CURLOPT_URL => $url,
-		    CURLOPT_RETURNTRANSFER => true,
-		    CURLOPT_POST => true,
-		    CURLOPT_POSTFIELDS => $params,
-		    CURLOPT_FOLLOWLOCATION => true
-		));
-		 
-		$data = curl_exec($ch);
-		 
-		curl_close($ch);
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $params,
+            CURLOPT_FOLLOWLOCATION => true,
+        ));
 
-		return $data;
-	}
+        $data = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $data;
+    }
 }
-
-
-?>
